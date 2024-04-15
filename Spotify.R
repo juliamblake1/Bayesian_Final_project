c27daafcd1bf1876d524b952755e353215b7f8df
juliamblake1 <91092063+juliamblake1@users.noreply.github.com>
2024-04-15 10:39:17 -0400

    Add files via upload

--- Spotify.R
+++ Spotify.R
@@ -1,6 +1,11 @@
 #Spotify Popularity Data https://www.kaggle.com/datasets/iamsumat/spotify-top-2000s-mega-dataset/data
+library(corrplot)
+library(brms)
+set.seed(123)
 
-setwd("C:/Users/JULIA_BLAKE/OneDrive - S&P Global/Columbia/Spring 2024/Bayesian/Final Project")
+
+
+#setwd("C:/Users/JULIA_BLAKE/OneDrive - S&P Global/Columbia/Spring 2024/Bayesian/Final Project")
 data <- read.csv("Spotify-2000.csv")
 
 #Data Cleaning
@@ -19,7 +24,7 @@ data$Duration <- as.integer(gsub(",", "", data$Duration))
 ggplot(data,aes(y=Popularity)) + geom_boxplot() +
   labs(title="Distribution of Song Popularity", subtitle="Spotify, April 2019")
 
-#Clean up Genre Column
+#Clean up Genre Column (not sure if we want to use this but tried to consolidate)
 data$Genre[grepl("pop", data$Genre, ignore.case = TRUE)] <- "pop"
 data$Genre[grepl("punk", data$Genre, ignore.case = TRUE)] <- "punk"
 data$Genre[grepl("rock|prog", data$Genre, ignore.case = TRUE)] <- "rock"
@@ -38,14 +43,20 @@ data$Genre[grepl("indie", data$Genre, ignore.case = TRUE)] <- "indie"
 #New Column 
 data$yrs_since_release <- 2019 - data$Year
 
-
 #EDA
 par(mfrow=c(1,1))
 cor_matrix <- cor(data[, 5:15])
 corrplot(cor_matrix, method = "color", type = "upper", order = "hclust", 
          tl.col = "black", tl.srt = 45, diag = FALSE, addCoef.col = "black", number.cex = 0.6)
 
+numeric_vars <- sapply(data, is.numeric)
+numeric_data <- data[, numeric_vars]
 
+par(mfrow = c(3, ceiling(sum(numeric_vars) / 3))) # Corrected the function to ceiling()
+
+for (i in 1:ncol(numeric_data)) {
+  hist(numeric_data[, i], main = names(numeric_data)[i], xlab = "", col = "skyblue", border = "white")
+}
 
 #OLS
 # Assuming 'response_variable' is the dependent variable and 'predictor_variable1', 'predictor_variable2', etc. are the independent variables
@@ -55,4 +66,19 @@ model <- lm(Popularity ~ BPM + Energy + Danceability + Loudness_db +
               Liveness + Valence + Duration + Acousticness + Speechiness + yrs_since_release, 
             data = data)
 summary(model)
+
+# Bayesian estimation
+brm_model <- brm(Popularity ~ BPM + Energy + Danceability + Loudness_db + 
+                   Liveness + Valence + Duration + Acousticness + Speechiness + yrs_since_release, 
+                 data = data) # can add MCMC settings
+
+# Summary of the Bayesian regression model
+summary(brm_model)
+plot(brm_model)
+stancode(brm_model)
+
+
+####################################
+
+#try diff prior than default
 
